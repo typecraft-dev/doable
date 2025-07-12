352a3b48128bc3d260b00da5701dee7a2ea93cf7
Christopher Power <typecraft@Christophers-MacBook-Pro.local>
2025-07-12 01:12:09 -0400

    Add all crud actions by hand for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,47 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Project updated successfully!"
+      redirect_to project_path(@project)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project deleted."
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.expect(project: [ :name ])
+  end
 end
